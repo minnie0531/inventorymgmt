9339851ec64bf67e6b23291beb8a673ca7fd7ef7
root <root@minnie.garagekr.com>
2021-11-03 08:21:58 -0500

    [TASK] Add OpenAPI documentation and test endpoints

--- src/main/java/com/ibm/inventorymgmt/controller/InventoryController.java
+++ src/main/java/com/ibm/inventorymgmt/controller/InventoryController.java
@@ -1,6 +1,8 @@
 package com.ibm.inventorymgmt.controller;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +16,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
@@ -21,27 +28,29 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "inventory", description = "endpoints for inventory")
 @ComponentScan(basePackages = {"com.ibm.inventorymgmt"})
 public class InventoryController {
-    
+
     private static final Logger logger = LoggerFactory.getLogger(InventoryController.class);
 
     @Autowired
     private StringRedisTemplate redisTemplate;
-    
-    
+
     @GetMapping("/")
     @Hidden
     public String index() {
         return "Greetings from Spring Boot!";
     }
-    
-   //변경 : 재고 - 현재 주
+
+
+    @Operation(summary = "Decreament inventory by given number. It means an order has been started")
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Success", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))})})
     @GetMapping("/order")
     public String order(@RequestParam String productNo, @RequestParam int number) {
         HashOperations<String, Object,  Object> hashOperations = redisTemplate.opsForHash();
         int numOfprod = this.inquiry(productNo);
-        
+
         logger.info("Current number of product : %", numOfprod);
-        
+
         if( numOfprod > 0) {
             hashOperations.put(productNo, "number", Integer.toString(numOfprod-number));
             return productNo + " is ordered";
@@ -49,8 +58,11 @@ public class InventoryController {
             return productNo + " is not available";
         }
     }
+
     
-    //변경: 재고 + 현재취
+    @Operation(summary = "Increament inventory by given number. It means an order cancellation has been started")
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Success", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))})})
     @GetMapping("/cancel")
     public String cancel(@RequestParam String productNo, @RequestParam int number) {
         HashOperations<String, Object,  Object> hashOperations = redisTemplate.opsForHash();
@@ -62,6 +74,9 @@ public class InventoryController {
     }
     
 
+    @Operation(summary = "The number of products")
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Success", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))})})
     @GetMapping("/inquiry")
     public int inquiry(@RequestParam String productNo) {
         HashOperations<String, Object, Object> hashOperations = redisTemplate.opsForHash();
@@ -69,5 +84,21 @@ public class InventoryController {
         return Integer.valueOf((String)entries.get("number"));
     }
 
+    @Operation(summary = "Prodcuts list")
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Success", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))})})
+    @GetMapping("/products")
+    public Set<String> productList(){
+        return redisTemplate.keys("*");
+    }
+
+    @Operation(summary = "Register a new products - test")
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Success", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))})})
+    @GetMapping("/registration")
+    public void CreateNewProduct(@RequestParam String productNo, @RequestParam int numOfProd) {
+        HashOperations<String, Object,  Object> hashOperations = redisTemplate.opsForHash();
+        hashOperations.put(productNo, "number", Integer.toString(numOfProd));
+    }
 }
 
